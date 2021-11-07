64ef1a73603b88d17fd0ad7dd10dae3c02b04550
Matija Osrečki <matija.osrecki@gmail.com>
2021-11-07 22:11:07 +0100

    novi half rjeseni lab2

--- zadace/dz2/zad3.py
+++ zadace/dz2/zad3.py
@@ -17,54 +17,66 @@ i vremenski interval (1:00, 1:20), optimalni izbor aktivnosti je skup {(’A’,
 '''
 
 
+lst = [[-4, 1], [1, 5], [2, 10], [3, 5], [1, 3], [3, 8], [8, 12], [5, 11]]
+lst = [[110, 120], [115, 119], [112, 114], [14, 155]]
+
+
+def longest(lst):
+    mx = (0, [])
+    for i in range(1, len(lst) - 1):    # test for all following and acceptable elements
+        if lst[i][0] == lst[0][1]:
+            add = longest(lst[i:])
+            if add[0] > mx[0]:         # keep "longest" chain
+                mx = add
+    #print(lst, mx)
+    return (lst[0][1] - lst[0][0] + mx[0], [lst[0]] + mx[1])
+
+
+# chain elements must be in increasing order
+# print(longest(sorted(lst)))
+
+
 def toMinutes(interval):
     sati, minute = interval
     return sati * 60 + minute
 
 
-def rangeMap(niz):
-    range_map = {}
-    for akt, poc, kraj in niz:
-        range_map[akt] = range(toMinutes(poc), toMinutes(kraj))
-    return range_map
+def aktivnostiIntervala(aktivnosti, interval):
+    akt_u_intervalu = []
+    minterval = (toMinutes(interval[0]), toMinutes(interval[1]))
+    for aktivnost in aktivnosti:
+        _, poc, kraj = aktivnost
+        mpoc = toMinutes(poc)
+        mkraj = toMinutes(kraj)
+        if mpoc >= minterval[0] and mkraj <= minterval[1]:
+            akt_u_intervalu.append(aktivnost)
+    return akt_u_intervalu
 
 
-def optimal(niz):
-    range_map = rangeMap(niz)
-    print(range_map)
+def optimal(aktivnosti, interval, rez=[]):
+    aktivnosti = aktivnostiIntervala(aktivnosti, interval)
+    vrijeme = toMinutes(interval[1]) - toMinutes(interval[0])
 
-    termini = []
-    range_list = list(range_map.items())
-    for i in range(0, len(range_list)):
-        akt = range_list[i]
-        optimalno = [niz[i]]
-        for j in range(i+1, len(range_list)):
-            slj_akt = range_list[j]
-            has_interesection = set(akt[1]).intersection(slj_akt[1])
-            if not has_interesection:
-                has_internal_intersection = False
-                for k in range(1, len(optimalno)):
-                    akt_ime = optimalno[k][0]
-                    if (akt_ime == slj_akt[0]):
-                        continue
-                    nova_akt = (akt_ime, range_map[akt_ime])
-                    has_interesection = set(
-                        slj_akt[1]).intersection(nova_akt[1])
-                    if has_interesection:
-                        has_internal_intersection = True
-                        break
+    for i in range(0, len(aktivnosti)):
+        _, poc, kraj = aktivnost = aktivnosti[i]
+        mpoc = toMinutes(poc)
+        mkraj = toMinutes(kraj)
+        novo_vrijeme = vrijeme - (mkraj - mpoc)
+        preklapanje = False
+        for r in rez:
+            _, rezpoc, rezkraj = r
+            rpoc = toMinutes(rezpoc)
+            rkraj = toMinutes(rezkraj)
+            if (set(range(rpoc, rkraj)).intersection(range(mpoc, mkraj))):
+                preklapanje = True
+                break
 
-                if not has_internal_intersection:
-                    optimalno.append(niz[j])
-        termini.append(optimalno)
-
-    max_termin = []
-    for termin in termini:
-        duljina = len(termin)
-        if duljina >= len((max_termin)):
-            max_termin = termin
-
-    return max_termin
+        if novo_vrijeme > 0 and not preklapanje:
+            vrijeme = novo_vrijeme
+            rez.append(aktivnost)
+        else:
+            break
+    return rez
 
 
 aktivnosti = [
@@ -74,16 +86,11 @@ aktivnosti = [
     ('R', (1, 4), (1, 55))
 ]
 
-# print(optimal(aktivnosti))
+# aktivnosti = [
+#     ('F', (110), (120)),
+#     ('G', (115), (119)),
+#     ('A', (112), (114)),
+#     ('R', (14), (155))
+# ]
 
-
-aktivnosti2 = [
-    ('F', (1, 10), (1, 20)),
-    ('G', (1, 15), (1, 19)),
-    ('A', (1, 12), (1, 14)),
-    ('R', (1, 4), (1, 55)),
-    ('H', (2, 4), (2, 56)),
-    ('Z', (2, 20), (2, 24)),
-]
-
-print(optimal(aktivnosti2))
+print(optimal(aktivnosti, ((1, 0), (1, 20))))
