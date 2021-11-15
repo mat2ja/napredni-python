6995ab2bcef30688cb5e61e224ca89f6b9867738
Matija Osrečki <matija.osrecki@gmail.com>
2021-11-15 21:23:01 +0100

    finish-ish regex lab3

--- zadace/dz3/dz.py
+++ zadace/dz3/dz.py
@@ -55,3 +55,68 @@ zen = 'Beautiful is [better] than (ugly). [Explicit is better than [implicit]. [
 rez_d = re.findall(r_d, zen)
 print('\nd)')
 print(rez_d)
+
+
+'''
+2. Za zadatke 1a i 1b nacrtajte konačne automate (deterministične ili nedeterminis-
+tične).
+'''
+
+'''
+3. Za regularni izraz (a*|b)(ab)* nacrtajte konačni automat i napišite tablicu prelaza.
+'''
+prijelazi_z3 = {
+    0: {'a': {0}, 'ø': {1}},
+    1: {'b': {2}},
+    2: {'a': {1}}
+}
+
+'''
+4. Definirajte regularni izraz za heksadecimalne brojeve.
+'''
+r4 = r'(0[xX])?[\da-fA-F]+'
+test_4_1 = '0x0f4'
+test_4_2 = '0acdadecf822eeff32aca5830e438cb54aa722e3'
+test_4_3 = '8BADF00D'
+rez_4_1 = re.fullmatch(r4, test_4_1)
+rez_4_2 = re.fullmatch(r4, test_4_2)
+rez_4_3 = re.fullmatch(r4, test_4_3)
+print('\n4)')
+print(rez_4_1)
+print(rez_4_2)
+print(rez_4_3)
+
+
+'''
+5. Definirajte regularni izraz za rimske brojeve.
+'''
+r_5 = r'^M*(C[MD]|D?C{0,3})(X[CL]|L?X{0,3})(I[XV]|V?I{0,3})$'
+test_5 = 'MMMCMXCIX'
+rez_5 = re.match(r_5, test_5)
+print('\n5)')
+print(rez_5.group(0))
+
+'''
+6. Za konačni automat na slici 1.7 napišite odgovarajući regularni izraz.
+'''
+r_6 = r'(a?a+bb{1,})*'
+test_6_1 = 'aaabb'
+test_6_2 = 'abb'
+
+'''
+7. Nacrtajte konačni automat (deterministični ili nedeterministični) i napišite odgo-
+varajući regularni izraz za nizove znamenki gdje se niti jedna znamenka ne ponavlja.
+'''
+prijelazi_z7 = {}
+r_7 = r''
+
+
+'''
+8. Implementirajte konačni automat sa slike 1.4 pomoću tablice prelaza.
+'''
+prijelazi_z8 = {
+    0: {'a': {0}, 'b': {0}},
+    1: {'a': {1}, 'b': {1}},
+    2: {'a': {2}, 'b': {2}, 'e': {0}},
+    3: {},
+}
